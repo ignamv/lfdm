073f905123c73f8a2b5339d275f505c8d2ca5a1d
Ignacio Martinez V <ignamv@gmail.com>
2015-01-22 19:00:36 -0300

    Improve taskwrap error handling

--- modulos/taskwrap.py
+++ modulos/taskwrap.py
@@ -2,14 +2,27 @@ import asyncio
 import logging
 import functools
 from io import StringIO
+import sys
+import traceback
+import io
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+console = logging.StreamHandler()
+console.setLevel(logging.ERROR)
+logger.addHandler(console)
 
 def task_died(handle):
     """Log dead task's stack trace"""
-    stack = StringIO()
-    handle.print_stack(file=stack)
-    logger.debug(stack.getvalue())
+    exception = handle.exception()
+    if exception is None:
+        logger.debug('Task finished: %s', str(handle))
+        return
+    logger.error('Task DIED: %s', str(handle))
+    stream = io.StringIO()
+    handle.print_stack(file=stream)
+    logger.error(stream.getvalue())
+    logger.error('%s: %s', str(type(exception)), str(exception))
 
 def taskwrap(fn):
     """Wrap coroutine. Resulting function starts coroutine in event loop."""
@@ -17,6 +30,7 @@ def taskwrap(fn):
 
     @functools.wraps(fn)
     def create_task(*args, **kwargs):
+        logger.debug('Create task %s', fn.__name__)
         loop = asyncio.get_event_loop()
         task = asyncio.async(coroutine(*args, **kwargs))
         task.add_done_callback(task_died)
