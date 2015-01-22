b0802c7fa7a08135a48a3658ab5ddd408ddba8b1
Ignacio Martinez V <ignamv@gmail.com>
2015-01-22 19:00:17 -0300

    Add finalization to LantzInitializeDialog

--- lantzinitializedialog.py
+++ lantzinitializedialog.py
@@ -1,35 +1,72 @@
-from PyQt4.QtGui import QDialog, QTableWidget, QVBoxLayout
+import asyncio
+from PyQt4.QtGui import QDialog, QTableWidget, QVBoxLayout, QTableWidgetItem
 from PyQt4.QtCore import Qt
-from lantz.ui.widgets import initialize_and_report
+import taskwrap
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class LantzInitializeDialog(QDialog):
-    def __init__(self, instruments, parent=None):
+    def __init__(self, instruments, parent=None, finalize=False):
         super().__init__(parent)
+        self.finalize = finalize
+        self.prefix = ['Initial', 'Final'][finalize]
         self.setLayout(QVBoxLayout())
-        self.setWindowTitle('Initialize instruments')
+        self.setWindowTitle(self.prefix + 'ize instruments')
         self.setWindowModality(Qt.WindowModal)
 
-        self.table = QTableWidget(0, 3)
-        self.table.setHorizontalHeaderLabels(["Name", "Class", "Status"])
+        self.table = QTableWidget(len(instruments), 2)
+        self.instruments = instruments
+        for ii, inst in enumerate(instruments):
+            self.table.setItem(ii, 0, QTableWidgetItem(inst.name))
+            self.table.setItem(ii, 1, QTableWidgetItem('Pending...'))
+
+        self.table.setHorizontalHeaderLabels(["Name", "Status"])
         self.table.verticalHeader().setVisible(False)
-        self.table.resize(250, 50)
         self.table.resizeColumnToContents(0)
         self.layout().addWidget(self.table)
+        self.initialize_all()
 
-        self.instruments = instruments
+    @asyncio.coroutine
+    def initialize_instrument(self, instrument):
+        logger.debug(self.prefix + 'izing %s', instrument.name)
+        row = self.instruments.index(instrument)
+        try:
+            yield from instrument._submit([instrument.initialize,
+                instrument.finalize][self.finalize])
+        except Exception as e:
+            logger.error('%sizing %s: %s', self.prefix, instrument.name,
+                    str(e))
+            self.table.item(row, 1).setText(str(e))
+            return False
+        self.table.item(row, 1).setText('Done')
+        return True
 
-    def showEvent(self, ev):
-        super().showEvent(ev)
-        self.thread = initialize_and_report(self.table, self.instruments)
-        self.thread.finished.connect(self.close)
+    @taskwrap.taskwrap
+    def initialize_all(self):
+        futures = [asyncio.async(self.initialize_instrument(inst)) 
+                    for inst in self.instruments]
+        results = yield from asyncio.gather(*futures)
+        if all(results):
+            self.close()
 
 if __name__ == '__main__':
     from PyQt4.QtGui import QApplication
+    import quamash
     from keithley220 import Keithley220
     from os import sys
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    logger.addHandler(console)
 
     app = QApplication(sys.argv)
-    keith = Keithley220('GPIB0::12::INSTR')
-    ui = LantzInitializeDialog([keith])
+    loop = quamash.QEventLoop(app)
+    asyncio.set_event_loop(loop) 
+    instruments = [
+        Keithley220('GPIB0::12::INSTR'),
+        #Keithley220('GPIB0::3::INSTR'),
+        ]
+    ui = LantzInitializeDialog(instruments)
     ui.show()
-    exit(app.exec_())
+    exit(loop.run_forever())
