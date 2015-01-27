d7f1563badfae0f8e04e905a016a25c28aa78f0f
Ignacio Martinez V <ignamv@gmail.com>
2015-01-27 18:22:47 -0300

    Arbitrary length pulses with CV_Pulsada

--- cv_pulsada.py
+++ cv_pulsada.py
@@ -1,13 +1,66 @@
-from hp8112a import HP8112A
+from lantz import Q_
+from time import sleep
+import asyncio
 
-def pulso(inst, largo, signo=1, **kwargs):
-    """Manda un pulso de largo arbitrario
+class CV_Pulsada(object):
+    def __init__(self, generador, osciloscopio):
+        self.gen = generador
+        self.osc = osciloscopio
+        self.setAncho(Q_(1., 's'))
 
-    signo puede ser 1 o -1
-    Los argumentos adicionales configuran el generador. Ejemplo:
-        
-    >>> pulso(inst, Q_(10, 's'), signo=-1, trailing_edge=Q_(2, 'ms'))
-    """
-    inst.trigger_mode = 'trigger'
-    # Esto genera una transición rápida 
-    inst.complement = signo
+    def setAncho(self, ancho):
+        self._ancho = ancho
+        if ancho < Q_(950, 'ms'):
+            self.gen.update_async(width = ancho)
+            self.manual = False
+        else:
+            self.manual = True
+
+    def ancho(self):
+        return self._ancho
+
+    @asyncio.coroutine
+    def pulso(self):
+        """Genera un pulso de ancho configurable
+
+        La perilla LEVEL del generador debe estar hacia el límite izquierdo"""
+        if self.manual:
+            yield from self.pulso_manual()
+        else:
+            yield from self.pulso_normal()
+
+    @asyncio.coroutine
+    def pulso_manual(self):
+        # Para no pedirlo en medio del pulso
+        leading, trailing = yield from self.gen.refresh_async(
+                ['leading_edge', 'trailing_edge'])
+        # Evito glitches configurando en este orden
+        yield from self.gen.update_async(trigger_control = 'negative')
+        yield from self.gen.update_async(trigger_mode = 'external_width')
+        yield from self.gen.update_async(trigger_control = 'positive')
+        yield from self.sleep(self._ancho)
+        yield from self.gen.update_async(trigger_control = 'negative')
+        yield from self.sleep(trailing)
+
+    @asyncio.coroutine
+    def pulso_normal(self):
+        # Para no pedirlo en medio del pulso
+        leading, trailing = yield from self.gen.refresh_async(
+                ['leading_edge', 'trailing_edge'])
+        yield from self.gen.update_async(trigger_mode = 'trigger')
+        yield from self.gen.trigger_async()
+        yield from self.sleep(leading + trailing + self._ancho)
+
+    @asyncio.coroutine
+    def sleep(self, interval):
+        yield from asyncio.sleep(Q_(interval).to('s').magnitude)
+
+if __name__ == '__main__':
+    from hp8112a import HP8112A
+    from gwinstekgds2062 import GwinstekGDS2062
+    from lantz import initialize_many
+
+    gen = HP8112A('GPIB0::11::INSTR')
+    osc = GwinstekGDS2062('ASRL5::INSTR')
+    initialize_many([gen, osc])
+    cv = CV_Pulsada(gen, osc)
--- cv_pulsada_ui.py
+++ cv_pulsada_ui.py
@@ -1,17 +1,36 @@
 from hp8112a import HP8112A
 from gwinstekgds2062 import GwinstekGDS2062
 from PyQt4.uic import loadUiType
+from PyQt4.QtCore import pyqtSlot
+from lantzinitializedialog import LantzInitializeDialog
+from taskwrap import taskwrap
+from cv_pulsada import CV_Pulsada
+from lantz import Q_
 
 form, base = loadUiType('cv_pulsada.ui')
 
-def CV_Pulsada_UI(base):
+class CV_Pulsada_UI(base):
     def __init__(self, parent=None):
-        super().__init__(self, parent)
+        super().__init__(parent)
         self.ui = form()
         self.ui.setupUi(self)
+        self.gen = HP8112A('GPIB0::11::INSTR')
+        self.osc = GwinstekGDS2062('ASRL5::INSTR')
+        init = LantzInitializeDialog([self.gen, self.osc], parent=self)
+        init.show()
+        self.cv = CV_Pulsada(self.gen, self.osc)
+
+    @pyqtSlot()
+    @taskwrap
+    def on_pulso_clicked(self):
+        self.cv.setAncho(Q_(self.ui.ancho.text()).to('s'))
+        yield from self.cv.pulso()
+        
 
 if __name__ == '__main__':
     import sys
+    import logging
+    import asyncio
     from PyQt4.QtGui import QApplication
     from PyQt4.QtCore import QCoreApplication
     QCoreApplication.setOrganizationName('LFDM')
@@ -21,6 +40,6 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     loop = QEventLoop(app)
     asyncio.set_event_loop(loop) 
-    ui = CV_Pulsada_GUI()
+    ui = CV_Pulsada_UI()
     ui.show()
     exit(loop.run_forever())
