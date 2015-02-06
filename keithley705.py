3b87a74b3736a206ba48e905d7ab7ede7566e1a1
Ignacio Martinez V <ignamv@gmail.com>
2015-02-06 17:08:48 -0300

    Fix Keithley705 channel cache

--- keithley705.py
+++ keithley705.py
@@ -1,15 +1,21 @@
 from lantz import Feat, DictFeat, Q_, Action
 from lantz.visa import GPIBVisaDriver
 
+columns = list(range(1,6))
+rows = list(range(1,5))
 class Keithley705 (GPIBVisaDriver):
     @Action()
     def matrix_mode(self):
         self.send('A0')
 
-    @DictFeat(values={True: True, False: False},
-            keys=[(col, row) for col in range(1, 6) for row in range(1, 5)])
+    columns = columns
+    rows = rows
+
+    @DictFeat(read_once=True, values={True: True, False: False},
+            keys=[(col, row) for col in columns 
+                             for row in rows])
     def channel(self, key):
-        return self.channels[key]
+        return self.query('B{:02d}{:01d}U0X'.format(col, row))[-1] == '1'
 
     @Action()
     def open(self, channel):
@@ -30,16 +36,26 @@ class Keithley705 (GPIBVisaDriver):
         data = self.query('U1X').split(',')
         for channel, status in zip(data[::2], data[1::2]):
             col, row = channel.split(':')
-            ret[(int(col), int(row))] = (status == '1')
+            key = (int(col), int(row))
+            ret[key] = (status == '1')
+            self._lantz_features['channel'].set_cache(self, ret[key], key)
         return ret
 
     @Action()
     def execute(self):
         self.send('X')
 
+    @Feat()
+    def status(self):
+        return '705' + self.query('U4X')
+
     @Action()
     def reset(self):
         self.send('RX')
+        for row in self.rows:
+            for column in self.columns:
+                self._lantz_features['channel'].set_cache(self, False, 
+                        (column, row))
 
     def initialize(self):
         super().initialize()
@@ -55,9 +71,10 @@ if __name__ == '__main__':
     log_to_screen(DEBUG)
     inst = Keithley705('GPIB0::29::INSTR')
     inst.initialize()
+    inst.reset()
     inst.channel[(4,3)] = True
     inst.channel[(2,1)] = True
-    chan = inst.channels
+    chan = inst.refresh('channel')
     print(chan)
     print([k for k in chan.keys() if chan[k]])
     inst.reset()
