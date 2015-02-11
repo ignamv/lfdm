eb9cfe48963694cb759586908af8ad050ebed2c7
Ignacio Martinez V <ignamv@gmail.com>
2015-02-11 17:38:37 -0300

    Fix get_save_filename for files without extension

--- autoincrement_file.py
+++ autoincrement_file.py
@@ -8,9 +8,9 @@ def get_save_filename(*args, **kwargs):
             directory=qset.value('path', ''), **kwargs)
     if filename != '':
         # If filename ends with a counter, increment it and save as default
-        match = re.match(r'(.*)_(\d\d\d)\.(.*)$', filename)
+        match = re.match(r'(.*)_(\d\d\d)(.*)$', filename)
         if match:
-            qset.setValue('path', '{}_{:03d}.{}'.format(match.group(1),
+            qset.setValue('path', '{}_{:03d}{}'.format(match.group(1),
                 int(match.group(2)) + 1, match.group(3)))
         else:
             qset.setValue('path', re.sub(r'(\.\w+|)$', r'_001\1', filename))
