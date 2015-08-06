3d82c448ebf9f3864d75531a48f91d277b647e22
Ignacio Martinez Vazquez <ignamv@gmail.com>
2015-08-06 15:02:51 -0300

    cv.py: Parse command line arguments

--- cv.py
+++ cv.py
@@ -1,13 +1,31 @@
 # encoding: utf-8
-from lantz import Q_
+import argparse
 import numpy as np
 import sys
 from time import sleep
 from datetime import datetime
 
+from lantz import Q_
 from keithley705 import Keithley705
 from gwinsteklcr8110g import GwinstekLCR8110G
 from hp4277a import HP4277A
+from util import linspace, arange
+
+parser = argparse.ArgumentParser()
+parser.add_argument('salida')
+parser.add_argument('--volver', help='Medir ida y vuelta',
+    action='store_true')
+parser.add_argument('--vinicial', type=Q_, help='Tensión inicial',
+        required=True)
+parser.add_argument('--vfinal', type=Q_, help='Tensión final', required=True)
+parser.add_argument('--frecuencia', type=Q_, help='Frecuencia del LCR',
+        default=Q_(995, 'kHz'))
+parser.add_argument('--pausa', type=Q_, default=Q_(10, 'ms'),
+        help='Pausa entre cambio de bias y medición')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--vpaso', type=Q_, help='Aumento de tensión entre pasos')
+group.add_argument('--pasos', type=int, help='Cantidad de pasos')
+args = parser.parse_args()
 
 # Me conecto a los instrumentos
 matriz = Keithley705('GPIB0::29::INSTR')
@@ -15,9 +33,12 @@ matriz.initialize()
 
 lcr = GwinstekLCR8110G(port=0, timeout=5)
 lcr.initialize()
+lcr.drive_level = Q_(20, 'mV')
 
 bias_source = HP4277A('GPIB0::17::INSTR')
 bias_source.initialize()
+#lcr = bias_source
+#lcr.drive_level = 'low'
 
 # Configuro la matriz
 matriz.matrix_mode()
@@ -32,47 +53,40 @@ matriz.execute()
 
 # Configuro el LCR
 lcr.equivalent_circuit = 'parallel'
-lcr.frequency = Q_(1, 'MHz')
-lcr.drive_level = Q_(2, 'mV')
+lcr.frequency = args.frecuencia
 
-archivo = r'..\mediciones\vi_silicio\cv {}.raw'.format(datetime.today().strftime( '%Y-%m-%d_%H.%M.%S'))
-salida_raw = open(archivo, 'w', encoding='utf-8')
-print('Salida raw: ' + archivo)
-archivo = r'..\mediciones\vi_silicio\cv {}.txt'.format(datetime.today().strftime( '%Y-%m-%d_%H.%M.%S'))
-salida = open(archivo, 'w', encoding='utf-8')
-print('Salida linda: ' + archivo)
-salida_raw.write('# {}\n'.format(' '.join(sys.argv)))
-salida_raw.write('# Bias: ' + bias_source.learn_raw + '\n')
-salida_raw.write('# Bias: ' + bias_source.learn_raw + '\n')
-salida_raw.write('# LCR: {} {} {}\n'.format(lcr.recall('equivalent_circuit'),
+salida = open(args.salida, 'w', encoding='utf-8')
+print('Salida: ' + args.salida)
+salida.write('# {}\n'.format(' '.join(sys.argv)))
+salida.write('# Bias: ' + bias_source.learn_raw + '\n')
+salida.write('# Bias: ' + bias_source.learn_raw + '\n')
+salida.write('# LCR: {} {} {}\n'.format(lcr.recall('equivalent_circuit'),
     lcr.recall('frequency'), lcr.recall('drive_level')))
-salida_raw.write('# Matriz: {}\n'.format(matriz.status))
-tensiones = Q_(np.linspace(-1., 1., 40), 'V')
+salida.write('# Matriz: {}\n'.format(matriz.status))
+if args.vpaso:
+    tensiones = arange(args.vinicial, args.vfinal, args.vpaso).to('V')
+else:
+    tensiones = linspace(args.vinicial, args.vfinal, args.pasos).to('V')
+if args.volver:
+    tensiones = Q_(np.concatenate((tensiones, tensiones[::-1])), 'V')
 
-salida_raw.write('# Tensión [V]\tC [F]\tR [ohm]\n')
-salida.write('# Tensión [V]\tC [F]\tsigmaC[F]\tR [ohm]\tsigmaR[ohm]\n')
-bias_source.bias_voltage = tensiones[0]
-sleep(1)
-repeticiones_max = 10
-mediciones = np.empty((repeticiones_max, 2))
-for ii, bias in enumerate(tensiones):
-    print('{}/{}'.format(ii + 1, len(tensiones)), end='', flush=True)
-    bias_source.bias_voltage = bias
-    mediciones[0, :] = lcr.measure
-    for jj in range(1, repeticiones_max):
-        salida_raw.write('{}\t{}\t{}\n'.format(
-            bias.magnitude, mediciones[jj - 1, 0], mediciones[jj - 1, 1]))
-        mediciones[jj, :] = lcr.measure
+salida.write('# Tensión [V]\tC [F]\tR [ohm]\tTiempo[s]\n')
+try:
+    bias_source.bias_voltage = tensiones[0]
+    sleep(1)
+    for ii, bias in enumerate(tensiones):
+        print('{}/{}: {}'.format(ii + 1, len(tensiones), bias), end='', 
+                flush=True)
+        print('bias...', end='', flush=True)
+        bias_source.bias_voltage = bias
+        print('pause...', end='', flush=True)
+        sleep(args.pausa.to('s').magnitude)
+        print('medir...', end='', flush=True)
         c, r = lcr.measure
-        if np.linalg.norm(mediciones[jj,:]-mediciones[jj-1,:]) \
-                / np.linalg.norm(mediciones[jj,:]) < .1:
-            break
-        print('.', end='', flush=True)
-    salida.write('{}\t{}\t{}\t{}\t{}\n'.format(
-        bias.magnitude, np.mean(mediciones[:jj+1,0]), 
-        np.std(mediciones[:jj+1,0]), np.mean(mediciones[:jj+1,1]), 
-        np.std(mediciones[:jj+1,1])))
-    print('')
-bias_source.bias_voltage = Q_(0, 'V')
-
-matriz.reset()
+        print('escribir...', end='', flush=True)
+        salida.write('{}\t{}\t{}\t{}\n'.format(bias.magnitude, c, r,
+            datetime.now().timestamp()))
+        print('')
+finally:
+    bias_source.bias_voltage = Q_(0, 'V')
+    matriz.reset()
